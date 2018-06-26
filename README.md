ee1cac319a406410385753db481df59e1060dd62
zer0rest <github@zer0rest.net>
2018-06-27 01:34:08 +0300

    Added ping.py and description for it on README

--- README.md
+++ README.md
@@ -8,4 +8,17 @@ _Τσίλιες_ backend is mainly written in `bash` and `python`.
 
 _Τσίλιες_ is still in a very early development stage where it's features are still being specced out. Check the issues page and feel free to comment or open an issue for a feature that you would like from an uptime monitoring platform but can't find in there.
 
-~zer0rest
+_Τσίλιες_ scripts use mqtt for message passing between them. Currently a public mqtt broker, `iot.eclipse.org`, provided by eclipse is used. _TODO: Set up custom mqtt broker using the `mosquitto` package and post instructions on how to do so._
+
+## Scripts
+
+### _ping.py_
+
+ping.py is responsible for pinging the monitored hosts. It listens to a topic defined in the
+`host_topic` variable for IP addresses to ping. When a new IP address is received it pings it and publishes it's latency and status at the topics defined in `host_latency_topic` and `host_status_topic` respectively. If the host does not respond after the 1 second ping timeout expires it sets it's status to `0` and it's latency to infinite or `INF`
+
+__TODO:__
+
+- Test the script with IPv6 Addresses. The script has only been tested with IPv4 addresses. While, issues are not expected with IPv6 addresses, it should be testes to ensure none arise.
+- Improve error handling. The script currently has no logic to inform the user if the connection to the mqtt broker failed. It also does not have any logic to inform the user if the domain name supplied at the ping command is malformed. Instead it behaves as if the host never responded.
+- Compartmentalise the code more in functions for improved readability.
---
+++ ping.py
@@ -0,0 +1,95 @@
+# This script is responsible for listening to a predefined mqtt topic for a host
+# to ping.
+# It implements threading so the host uptime check routine can happen without
+# blocking newer messages that may arrive while the routine is running.
+
+# Import the mqtt module, used for message passing between tsilies' scripts.
+import paho.mqtt.client as mqtt
+
+# Import the threading module.
+import threading
+
+#import the subprocess module, used to execute the ping command.
+import subprocess
+
+# Define the name of the monitoring agent.
+agent_nickname = "agent-1"
+
+# Define the topic where the script will listen for hosts to ping.
+host_topic = "tsilies/" + agent_nickname + "/to_ping/"
+
+# Define the domain/ip of the mqtt server.
+mqtt_broker = "iot.eclipse.org"
+
+# Create the function to ping the hosts and publish the latency and host status
+# in the predefined topics.
+# This is the function that will be executed at the on_message callback.
+def toPing(client, userdata, message):
+
+    # Define the variable where the host domain/ip will be stored.
+    host_ip = str(message.payload)
+
+    # Print the host to-be-pinged
+    print "Received message to ping: " + host_ip
+
+    # Define the host latency reporting topic.
+    host_latency_topic = "tsilies/" + agent_nickname + "/latency/" + host_ip + "/"
+
+    # Define the host status reporting topic.
+    host_status_topic = "tsilies/" + agent_nickname + "/status/" + host_ip + "/"
+
+    # Define ping command to execute
+    ping_cmd = "ping -c 3 -i 0.2 -W 1 " + host_ip + " | grep rtt | awk '{ printf $ 4 }' | cut -d'/' -f2 "
+
+    # Execute the ping command
+    ping = subprocess.Popen(ping_cmd, shell=True, stdout=subprocess.PIPE)
+
+    # Wait for the process to terminate and collect the standard output.
+    output, error = ping.communicate()
+
+    # Check if the host responded to pings or not.
+    # If it did, collect the latency.
+    if output == "":
+        # Set the host status to 0 which equals "Down"
+        status = "0"
+
+        # Set the latency to "infinite"
+        latency = "INF"
+
+        # Publish the host status to "host_status_topic"
+        client.publish(host_status_topic, status)
+
+        # Publish the latency status to "host latency_topic"
+        client.publish(host_latency_topic, latency)
+
+    else:
+        # Set the host status to 0 which equals "Down"
+        status = "1"
+
+        # Set the host status to 0 which equals "Down"
+        # .strip() is used to remove the blank line on the ping output.
+        latency = output.strip()
+
+        # Publish the host status to "host_status_topic"
+        client.publish(host_status_topic, status)
+
+        # Publish the latency status to "host latency_topic"
+        client.publish(host_latency_topic, latency.strip())
+
+
+# Create an instance of the mqtt client.
+client = mqtt.Client(agent_nickname)
+print "Created an instance of the mqtt client object."
+
+# Connect to the mqtt broker.
+client.connect(mqtt_broker)
+print "Connected to the mqtt client."
+
+# If the connection was successful, subscribe to the hosts-to-ping topic.
+client.subscribe(host_topic)
+
+# Define the function to call when a message is received on "host_topic".
+client.on_message = toPing
+
+# Make the script continuously listen for new messages.
+client.loop_forever()
